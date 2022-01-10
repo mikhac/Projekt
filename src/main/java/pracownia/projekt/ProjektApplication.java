029c1a69eb7e2318a5e8a7d45f558edf4445ce7b
Mikolaj Haczyk <mikhac@st.amu.edu.pl>
2022-01-10 23:44:39 +0100

    swagger_dziala

--- src/main/java/pracownia/projekt/ProjektApplication.java
+++ src/main/java/pracownia/projekt/ProjektApplication.java
@@ -2,12 +2,15 @@ package pracownia.projekt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+
+@EnableSwagger2
+//@EnableJpaRepositories("pracownia.projekt.repositories")
 @SpringBootApplication
 public class ProjektApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjektApplication.class, args);
 	}
-
 }
